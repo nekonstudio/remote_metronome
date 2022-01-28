2b077b24e29804b336af05a1ad630e17aa8e82bf
Sebastian Lisiecki <nekonstudio@gmail.com>
2022-01-28 15:51:18 +0100

    Playing metronome with sound from file, but there's problem with keeping tempo

---
+++ android/app/src/main/cpp/AAssetDataSource.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sebastian Lisiecki on 27/01/2022.
+//
+
+#include "AAssetDataSource.h"
---
+++ android/app/src/main/cpp/AAssetDataSource.h
@@ -0,0 +1,16 @@
+//
+// Created by Sebastian Lisiecki on 27/01/2022.
+//
+
+#ifndef ANDROID_AASSETDATASOURCE_H
+#define ANDROID_AASSETDATASOURCE_H
+
+
+
+class AAssetDataSource {
+
+};
+
+
+
+#endif //ANDROID_AASSETDATASOURCE_H
